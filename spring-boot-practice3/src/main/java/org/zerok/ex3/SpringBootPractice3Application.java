6b4c1159cae63b0e1903b28913adda7d88d2682c
nopecho <92011565+nopecho@users.noreply.github.com>
2021-12-01 19:12:39 +0900

    Add files via upload

---
+++ spring-boot-practice3/src/main/java/org/zerok/ex3/SpringBootPractice3Application.java
@@ -0,0 +1,13 @@
+package org.zerok.ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractice3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPractice3Application.class, args);
+    }
+
+}
---
+++ spring-boot-practice3/src/test/java/org/zerok/ex3/SpringBootPractice3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerok.ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPractice3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
