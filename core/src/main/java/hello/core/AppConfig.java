bafb4c0c83e6a04129445e668cad96d7530f3ba0
nopecho <ch.jooon@gmail.com>
2022-01-01 16:01:41 +0900

    리팩토링 : 역할,구현 나눔

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemmoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.core.order.orderServiceImpl;
 public class AppConfig { //AppConfig클래스에서 애플리케이션의 실제 동작에 필요한 구현체 생성 후 주입해줌, DI(의존관계 주입)
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemmoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemmoryMemberRepository();
     }
 
     public orderService orderService(){
-        return new orderServiceImpl(new MemmoryMemberRepository(),new FixDiscountPolicy());
+        return new orderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
