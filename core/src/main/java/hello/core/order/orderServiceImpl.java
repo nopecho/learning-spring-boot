9507f32df1f52a87df8732e041e8091c398c71ff
nopecho <ch.jooon@gmail.com>
2022-01-01 15:20:45 +0900

    의존 관계 코드 수정

--- core/src/main/java/hello/core/order/orderServiceImpl.java
+++ core/src/main/java/hello/core/order/orderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemmoryMemberRepository;
@@ -9,7 +10,8 @@ import hello.core.member.MemmoryMemberRepository;
 public class orderServiceImpl implements orderService{
 
     private final MemberRepository memberRepository = new MemmoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //할인 정책이 바뀐다면 OrderServiceImpl코드를 고쳐야함
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
