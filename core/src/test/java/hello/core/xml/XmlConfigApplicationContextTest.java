b8a22f63f8b5b8373a59ad23cc003af206684f87
nopecho <ch.jooon@gmail.com>
2022-01-05 22:35:38 +0900

    테스트 : xml 스프링 컨테이너 빈 조회 xml파일로GenericXmlApplicationContext(스프링 컨테이너) 등록 하고 조회하기

---
+++ core/src/test/java/hello/core/xml/XmlConfigApplicationContextTest.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlConfigApplicationContextTest {
+    @Test
+    @DisplayName("XML 파일로 스프링 컨테이너에 빈 을 등록하고 조회할 수 있다")
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
