5b5b199cd5795bebda8f6c9d9404d5ec9b94acb6
nopecho <ch.jooon@gmail.com>
2022-01-07 23:51:13 +0900

    테스트 : 빈 메타정보 출력 테스트

---
+++ core/src/test/java/hello/core/beanfind/BeanDefinitionTest.java
@@ -0,0 +1,23 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 메타정보 출력")
+    void 빈_메타정보(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "/ beanDefinition = "+beanDefinition);
+            }
+        }
+    }
+}
