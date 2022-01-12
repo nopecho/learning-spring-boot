8ab8d30042567c1fa508a913d2d2e8f2ffb0656e
nopecho <ch.jooon@gmail.com>
2022-01-12 21:51:19 +0900

    추가 : Lombok 기능활용

--- core/src/main/java/hello/core/order/orderServiceImpl.java
+++ core/src/main/java/hello/core/order/orderServiceImpl.java
@@ -4,20 +4,21 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemmoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //클래스 내의 final이 붙어 있는 변수의 생성자 만들어줌
 public class orderServiceImpl implements orderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; //DIP를 지키기위해 인터페이스에만 의존하도록 변경, BUT 구현체가 없기때문에 NPE발생!
 
-    @Autowired
-    public orderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { //외부에서 생성자를 통해 구현체를 주입해줌
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public orderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { //외부에서 생성자를 통해 구현체를 주입해줌
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
