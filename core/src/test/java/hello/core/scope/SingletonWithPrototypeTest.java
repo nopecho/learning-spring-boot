f7d539e01d2225f8f4a9764bcdd967a22c484562
nopecho <ch.jooon@gmail.com>
2022-01-15 20:28:57 +0900

    Test : Javax Provider Using

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -32,7 +33,7 @@ public class SingletonWithPrototypeTest {
         assertThat(clientBean1.logic()).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        assertThat((clientBean2.logic())).isEqualTo(2);
+        assertThat((clientBean2.logic())).isEqualTo(1);
 
 
     }
@@ -45,6 +46,9 @@ public class SingletonWithPrototypeTest {
         @Autowired //ObjectProvider는 빈을 생성안해도 스프링 컨테이너에서 알아서 주입해줌
         private ObjectProvider<PrototypeBean> beanObjectProvider; //오브젝트 프로바이더(ObjectProvider) <[원하는 빈 타입]>으로 ApplicationContext(DI 컨테이너)에서 찾아줌
 
+        @Autowired
+        private Provider<PrototypeBean> provider; //javax 라이브러리 사용 (이걸 사용하면 스프링 종속적이지 않음)
+
 //        @Autowired
 //        public ClientBean(PrototypeBean prototypeBean){
 //            this.prototypeBean = prototypeBean;
@@ -52,7 +56,8 @@ public class SingletonWithPrototypeTest {
 //        }
 
         public int logic(){
-            PrototypeBean prototypeBean = beanObjectProvider.getObject(); //프로바이더.getObject()로 스프링 컨테이너에서 해당 빈을 찾아서 반환함 DL(의존성 조회)
+//            PrototypeBean prototypeBean = beanObjectProvider.getObject(); //프로바이더.getObject()로 스프링 컨테이너에서 해당 빈을 찾아서 반환함 DL(의존성 조회)
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
