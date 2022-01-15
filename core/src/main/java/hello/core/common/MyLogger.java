cc2018294d2f75f314018c95d0075d931aa87023
nopecho <ch.jooon@gmail.com>
2022-01-15 21:51:57 +0900

    Refactor : ProxyMode 객체 사용

--- core/src/main/java/hello/core/common/MyLogger.java
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,8 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request") //request Scope 빈의 생명주기는 http요청이 오면 빈이 생성되고 요청이 끊기면 소멸됨
+@Scope(value = "request",proxyMode = ScopedProxyMode.TARGET_CLASS) //request Scope 빈의 생명주기는 http요청이 오면 빈이 생성되고 요청이 끊기면 소멸됨
+//proxyMode = 가짜 프록시 객체를 만들어서 스프링 빈에 미리 등록 시켜놓음, 그리고 실제 해당 객체를 사용시 실제 객체의 로직을 호출
 public class MyLogger {
 
     private String uuid;
--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -14,12 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; 
+    private final MyLogger myLogger; //MyLogger의 생명주기는 Http요청이 오고 응답시까지임. 그러므로 바로 스프링 컨테이너에서 MyLogger 빈을 주입 받을 수 없음! Provider의 지연 조회 기능 활용
+
 
     @RequestMapping("log-demo") // /log-demo <로 http요청이 오면 해당 메소드로 매핑됨
     @ResponseBody // 요청에 대한 http응답을 자바객체 -> 응답 body(본문) 객체에 넣어서 응답을 보냄
     public String logDemo(HttpServletRequest request){
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
