24c772eb53c9e2f78d6ac5f535e9dbbce5b6481d
nopecho <ch.jooon@gmail.com>
2022-01-16 18:27:54 +0900

    줄바꿈 수정

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -16,7 +16,6 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
     private final MyLogger myLogger; //MyLogger의 생명주기는 Http요청이 오고 응답시까지임. 그러므로 바로 스프링 컨테이너에서 MyLogger 빈을 주입 받을 수 없음! Provider의 지연 조회 기능 활용
 
-
     @RequestMapping("log-demo") // /log-demo <로 http요청이 오면 해당 메소드로 매핑됨
     @ResponseBody // 요청에 대한 http응답을 자바객체 -> 응답 body(본문) 객체에 넣어서 응답을 보냄
     public String logDemo(HttpServletRequest request){
