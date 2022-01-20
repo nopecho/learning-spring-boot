b3bf6a63d4b057788d83625a45bbc4e1fdbeee0f
nopecho <ch.jooon@gmail.com>
2022-01-21 02:15:36 +0900

    create : HTTP응답 바디(HTML)

---
+++ servlet/src/main/java/nopecho/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,26 @@
+package nopecho.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHtmlServlet",urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<div> 안녕하세요 </div>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
