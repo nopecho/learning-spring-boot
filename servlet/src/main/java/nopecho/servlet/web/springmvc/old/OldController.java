66e7cfb102891a22fad55160ecc04329681907e3
nopecho <ch.jooon@gmail.com>
2022-01-23 20:55:46 +0900

    SpringMVC 핸들러 매핑,어댑터 매핑 확인

---
+++ servlet/src/main/java/nopecho/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package nopecho.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller") //HandlerMapping에서 BeanNameUrl로 핸들러를 매핑할 수 있다. (핸들러 = 컨트롤러)
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
