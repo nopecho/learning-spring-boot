450a59936d86fec0bf08bf6b7d6cbbdde83c7997
nopecho <ch.jooon@gmail.com>
2022-01-25 21:04:34 +0900

    Request Parameter @ModelAttribute(Prameter To Object Binding)

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -82,6 +84,23 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap){ //Map으로 파라미터 정보 전부 가져오기 가능
         log.info("userName={}, userAge={}",paramMap.get("username"),paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){ //@ModelAttribute 애노테이션 붙이고 객체 타입을 받으면 스프링이 파라미터에 있는 객체를 생성해서 Requset파라미터의 값을 바인딩 시킴 (객체는 getter,setter를 가지고 있어야함) := DTO
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+        log.info("userName = {}, userAge = {}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){ //@ModelAttribute 생략가능
+        log.info("userName = {}, userAge = {}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
