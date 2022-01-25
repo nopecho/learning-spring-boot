30aa987da78dba382ed496b72a6cf5ce10f7c4a6
nopecho <ch.jooon@gmail.com>
2022-01-25 20:40:31 +0900

    Request Parameter Required

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -3,6 +3,8 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,4 +24,46 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
+
+    @ResponseBody //@ResponseBody 애노테이션이 붙어있는 컨트롤러 메서드는 리턴값을 바로 응답바디에 보냄
+    @RequestMapping("/request-param-v2")
+    public String requestParam(
+            @RequestParam("username") String name,
+            @RequestParam("age") int age
+    ){
+        log.info("userName={}, userAge={}",name,age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username, //매개변수명이랑 파라미터명이랑 같으면 생략 가능
+            @RequestParam int age
+    ){
+        log.info("userName={}, userAge={}",username,age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age){ //전부 생략도 가능
+        log.info("userName={}, userAge={}",username,age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username, //@RequestParam(required = true) 디폴트값, 필수로 있어야하는 파라미터 정보라는 뜻( 해당 파라미터가 없이 요청이 들어오면 4xx 에러 )
+            @RequestParam(required = false) Integer age){
+
+        log.info("userName={}, userAge={}",username,age);
+
+        return "ok";
+    }
+
 }
