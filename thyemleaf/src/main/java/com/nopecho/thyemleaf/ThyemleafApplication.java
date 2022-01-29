2b23a55c064d18b372e812d49736b1a383805354
nopecho <ch.jooon@gmail.com>
2022-01-29 16:57:27 +0900

    Init Project(create welcome page)

---
+++ thyemleaf/src/main/java/com/nopecho/thyemleaf/ThyemleafApplication.java
@@ -0,0 +1,13 @@
+package com.nopecho.thyemleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyemleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyemleafApplication.class, args);
+	}
+
+}
---
+++ thyemleaf/src/test/java/com/nopecho/thyemleaf/ThyemleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nopecho.thyemleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyemleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
