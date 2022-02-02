aa274bf78709d1f4c784e79601dcb750e0781d73
nopecho <ch.jooon@gmail.com>
2022-02-02 14:47:36 +0900

    Login Session 적용

--- item/src/main/java/com/nopecho/item/web/HomeController.java
+++ item/src/main/java/com/nopecho/item/web/HomeController.java
@@ -2,6 +2,7 @@ package com.nopecho.item.web;
 
 import com.nopecho.item.domain.member.Member;
 import com.nopecho.item.domain.member.MemberRepository;
+import com.nopecho.item.web.session.SessionManager;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,18 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
     public String home(){
         return "home";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String home2(@CookieValue(name = "memberId",required = false)Long memberId, Model model){
         if(memberId==null){
             return "home";
@@ -32,6 +36,18 @@ public class HomeController {
         }
 
         model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String home3(HttpServletRequest request, Model model){
+        Member member = (Member)sessionManager.getSession(request); //세션 정보가 없으면 로그인하지 않은 사용자로 분류
+
+        if(member == null){
+            return "home";
+        }
+
+        model.addAttribute("member",member);
         return "loginHome";
     }
 }
--- item/src/main/java/com/nopecho/item/web/login/LoginController.java
+++ item/src/main/java/com/nopecho/item/web/login/LoginController.java
@@ -2,6 +2,7 @@ package com.nopecho.item.web.login;
 
 import com.nopecho.item.domain.login.LoginService;
 import com.nopecho.item.domain.member.Member;
+import com.nopecho.item.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -22,13 +23,14 @@ import java.util.Arrays;
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginFrom(@ModelAttribute("loginForm") LoginForm form){
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -48,9 +50,30 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+    @PostMapping("/login")
+    public String loginV2(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        Member member = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(member == null){
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+        sessionManager.createSession(member,response);
+        return "redirect:/";
+    }
+
+//    @PostMapping("/logout")
     public String logout(HttpServletResponse response, HttpServletRequest request){
         extracted(response, Arrays.stream(request.getCookies()).findFirst().get().getName());
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request){
+        sessionManager.expire(request);
         return "redirect:/";
     }
 
