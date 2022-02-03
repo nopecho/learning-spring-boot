1364d771866bac15e96e10e5cd59856e6d824288
nopecho <ch.jooon@gmail.com>
2022-02-03 14:45:45 +0900

    ServletException API

---
+++ exception/src/main/java/nopecho/exception/api/ApiExceptionController.java
@@ -0,0 +1,34 @@
+package nopecho.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+    @GetMapping("api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id, HttpServletResponse response) throws IOException {
+        if(id.equals("ex")){
+            throw  new RuntimeException("잘못된 사용자");
+        }
+        if(id.equals("404")){
+            response.sendError(404,"404Errorrr!!");
+        }
+        return new MemberDto(id,"Nopecho");
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String memberId;
+        private String name;
+    }
+}
--- exception/src/main/java/nopecho/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/nopecho/exception/servlet/ErrorPageController.java
@@ -1,11 +1,17 @@
 package nopecho.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -31,6 +37,39 @@ public class ErrorPageController {
         log.info("ErrorPage 500 HandlerMethod 호출!");
         printErrorInfo(request);
         return "error-page/500";
+    }
+
+    /**
+     * @RequsetMapping(produces = MediaType) : 요청 requset의 Accept타입에 따라 어느 핸들러 메서드 호출할지 동적으로 결정
+     */
+    @ResponseBody
+    @RequestMapping(value = "error-page/500",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String,Object>> errorPage500api(HttpServletRequest request, HttpServletResponse response){
+
+        log.info("API ErrorPage 호출!");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status",request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "error-page/404",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String,Object>> errorPage404api(HttpServletRequest request, HttpServletResponse response){
+        log.info("API ErrorPage404 호출!");
+
+        Map<String, Object> result =  new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("ex",ex);
+        result.put("message", request.getAttribute(ERROR_MESSAGE));
+        result.put("status",request.getAttribute(ERROR_STATUS_CODE));
+
+        return new ResponseEntity<>(result,HttpStatus.valueOf((Integer) request.getAttribute(ERROR_STATUS_CODE)));
     }
 
     private void printErrorInfo(HttpServletRequest request){
