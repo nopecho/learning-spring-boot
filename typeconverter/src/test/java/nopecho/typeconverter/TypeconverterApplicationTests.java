fca1fcb75999aa4ad939e0d3750636b719063a28
nopecho <ch.jooon@gmail.com>
2022-02-03 18:11:35 +0900

    init project

---
+++ typeconverter/src/test/java/nopecho/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package nopecho.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
