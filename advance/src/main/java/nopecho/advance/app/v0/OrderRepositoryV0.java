163afddd9767c0ba8a73a97186185c94a1e5548e
nopecho <ch.jooon@gmail.com>
2022-02-13 19:48:43 +0900

    init

---
+++ advance/src/main/java/nopecho/advance/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,24 @@
+package nopecho.advance.app.v0;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@NoArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId){
+        if(itemId.equals("ex")){
+            throw new IllegalArgumentException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    public void sleep(int millis){
+        try{
+            Thread.sleep(millis);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ advance/src/main/java/nopecho/advance/app/v0/OrderServiceV0.java
@@ -0,0 +1,15 @@
+package nopecho.advance.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+
+    private final OrderRepositoryV0 orderRepository;
+
+    public void orderItem(String itemID) {
+        orderRepository.save(itemID);
+    }
+}
